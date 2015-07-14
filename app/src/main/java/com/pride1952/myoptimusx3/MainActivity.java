7db5a7ba3e2a18b22d90b1a967185d8be328e78e
jojo59516 <18510510879@163.com>
2015-07-15 02:01:45 +0800

    Signed-off-by: jojo59516 <18510510879@163.com>

--- app/src/main/java/com/pride1952/myoptimusx3/MainActivity.java
+++ app/src/main/java/com/pride1952/myoptimusx3/MainActivity.java
@@ -8,12 +8,16 @@ import android.graphics.ColorFilter;
 import android.graphics.ColorMatrixColorFilter;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.PopupWindow;
 import android.widget.Toast;
 
 
@@ -40,11 +44,13 @@ public class MainActivity extends Activity implements OnClickListener {
     private ImageButton mBtnRing;
     private ImageButton mBtnContact;
     private ImageButton mBtnMe;
+    private ImageButton mBtnPlus;
 
     private RingFragment mRing;
     private ContactFragment mContact;
     private MeFragment mMe;
 
+    PopupWindow mPopPlusMenu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,12 +59,15 @@ public class MainActivity extends Activity implements OnClickListener {
         mBtnRing=(ImageButton)findViewById(R.id.imgbtn_ring);
         mBtnContact=(ImageButton)findViewById(R.id.imgbtn_contact);
         mBtnMe=(ImageButton)findViewById(R.id.imgbtn_me);
+        mBtnPlus=(ImageButton)findViewById(R.id.imgbtn_plus);
 
         mBtnRing.setOnClickListener(this);
         mBtnContact.setOnClickListener(this);
         mBtnMe.setOnClickListener(this);
+        mBtnPlus.setOnClickListener(this);
 
         setDefaultFragment();
+        initPopupWindow();
     }
 
     private void setDefaultFragment()
@@ -73,10 +82,24 @@ public class MainActivity extends Activity implements OnClickListener {
         FragmentManager fm=getFragmentManager();
         FragmentTransaction trasaction=fm.beginTransaction();
         mRing=new RingFragment();
-        trasaction.replace(R.id.body,mRing);
+        trasaction.replace(R.id.body, mRing);
         trasaction.commit();
     }
 
+    private void initPopupWindow()
+    {
+        View view=this.getLayoutInflater().inflate(R.layout.popupwindow_plusmenu,null);
+        mPopPlusMenu=new PopupWindow(view, ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        mPopPlusMenu.setOutsideTouchable(true);
+
+        view.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                mPopPlusMenu.dismiss();
+            }
+        });
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -148,6 +171,20 @@ public class MainActivity extends Activity implements OnClickListener {
                 }
                 transaction.replace(R.id.body,mMe);
                 break;
+            case R.id.imgbtn_plus:
+                if(!mPopPlusMenu.isShowing())
+                {
+                    mPopPlusMenu.showAtLocation(findViewById(R.id.main), Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL,0,findViewById(R.id.bottombar).getHeight());
+                }
+                else
+                {
+                    mPopPlusMenu.dismiss();
+                }
+                break;
+            default:
+                mPopPlusMenu.dismiss();
+                break;
+
         }
         transaction.commit();
     }
